bc5506de7de8c20b6c153c9b0955571688215216
hirameghana01 <143598231+hirameghana01@users.noreply.github.com>
2023-08-30 15:10:34 +0530

    Update User_Authentication.js

--- User_Authentication.js
+++ User_Authentication.js
@@ -3,49 +3,71 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const User = require('../models/User'); // User model
 
+// Implement OTP generation logic (you can use a library like 'otp-generator')
+function generateOTP() {
+  // Generate OTP logic here
+  return '123456'; // Replace with actual OTP
+}
+
+// Implement token generation logic (you can use a library like 'jsonwebtoken')
+function generateAuthToken() {
+  // Generate token logic here
+  return 'your_auth_token'; // Replace with actual token
+}
+
 // Send OTP via email
 router.post('/send-otp', async (req, res) => {
   const { email } = req.body;
-  const otp = generateOTP(); // Implement OTP generation logic
+  const otp = generateOTP(); // Generate OTP
 
-  // Save OTP in the user's document or a separate OTP collection
-  await User.updateOne({ email }, { otp });
+  try {
+    // Save OTP in the user's document or a separate OTP collection
+    await User.updateOne({ email }, { otp });
 
-  // Send the OTP via email using nodemailer
-  const transporter = nodemailer.createTransport({
-    // Configure your email service here
-  });
+    // Send the OTP via email using nodemailer
+    const transporter = nodemailer.createTransport({
+      // Configure your email service here
+    });
 
-  const mailOptions = {
-    from: 'your_email@example.com',
-    to: email,
-    subject: 'Your OTP for Login',
-    text: `Your OTP: ${otp}`,
-  };
+    const mailOptions = {
+      from: 'your_email@example.com',
+      to: email,
+      subject: 'Your OTP for Login',
+      text: `Your OTP: ${otp}`,
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending OTP:', error);
-      res.status(500).json({ message: 'Failed to send OTP' });
-    } else {
-      res.json({ message: 'OTP sent successfully' });
-    }
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending OTP:', error);
+        res.status(500).json({ message: 'Failed to send OTP' });
+      } else {
+        res.json({ message: 'OTP sent successfully' });
+      }
+    });
+  } catch (error) {
+    console.error('Error sending OTP:', error);
+    res.status(500).json({ message: 'Failed to send OTP' });
+  }
 });
 
 // Validate OTP and login
 router.post('/login', async (req, res) => {
   const { email, otp } = req.body;
 
-  const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-  if (!user || user.otp !== otp) {
-    return res.status(401).json({ message: 'Invalid credentials' });
+    if (!user || user.otp !== otp) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate and send an authentication token upon successful login
+    const authToken = generateAuthToken(); // Generate token
+    res.json({ authToken });
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ message: 'An error occurred during login' });
   }
-
-  // Generate and send an authentication token upon successful login
-  const authToken = generateAuthToken(); // Implement token generation logic
-  res.json({ authToken });
 });
 
 module.exports = router;
